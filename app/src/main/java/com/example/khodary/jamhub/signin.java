9d09078ae2ec441ac8b5dcfd5d9673ab211d029b
sherifismail <sherif_diaa@aucegypt.edu>
2015-07-05 19:45:13 +0200

    Added 3 testing fragments

--- app/src/main/java/com/example/khodary/jamhub/signin.java
+++ app/src/main/java/com/example/khodary/jamhub/signin.java
@@ -4,13 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
-public class signin extends Activity {
+public class signin extends AppCompatActivity {
 
 Button signup;
     @Override
